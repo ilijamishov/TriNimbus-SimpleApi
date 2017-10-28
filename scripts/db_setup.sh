5c2feb179bafdf3687ee3b3e14310210a72d7dde
Ilija Mishov <ilija.mishov@gmail.com>
2017-10-28 02:34:01 +0200

    db_setup

--- scripts/db_setup.sh
+++ scripts/db_setup.sh
@@ -2,6 +2,15 @@
 
 export HOME=/home/ubuntu
 
+export DB_HOST=mdaav20beedz5f.c77ma5xmiwte.ca-central-1.rds.amazonaws.com
+export DB_NAME=MishovCF3DB
+export DB_PASS=mypass123
+export DB_PORT=5432
+export DB_USER=myuser
+
+
+
+
 cd /home/ubuntu/deployment/app
 
 dotnet add package Microsoft.EntityFrameworkCore.Design
